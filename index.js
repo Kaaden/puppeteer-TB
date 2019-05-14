ca9a52f19158fcb3ffbd43ebf0af934748ea389f
Kaaden <794102675@qq.com>
2019-05-14 15:42:57 +0800

    first

---
+++ index.js
@@ -0,0 +1,74 @@
+const express = require('express'), app = express(),
+    bodyParser = require('body-parser'), service = require('./service'),
+    urlencodedParser = bodyParser.urlencoded({ extended: true });
+let browser;
+let page;
+(async () => {
+    const puppeteer = require('puppeteer');
+    browser = await puppeteer.launch({ headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] });
+    page = await browser.newPage();
+})()
+
+app.use(bodyParser.json());
+//设置允许跨域 以及样式加载ContentType属性指定响应的 HTTP内容类型
+app.all('*', function (req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With');
+    res.header('Access-Control-Allow-Methods', 'PUT,POST,GET,DELETE,OPTIONS');
+    next();
+});
+app.get("/", function (req, res) {
+    res.send('Hello World!')
+})
+app.post("/fetchtb", urlencodedParser, function (req, res) {
+    const pageUrl = req.body.url;
+    if (pageUrl) {
+        (async () => {
+            const url = service.changeUrl(pageUrl)
+            try {
+                await page.setJavaScriptEnabled(true);
+                await page.goto(url);
+            } catch (error) {
+                service.resJosn({
+                    error: true,
+                    data: ""
+                }, res)
+
+                return
+            }
+
+            let objItem = ""
+            let error = false
+            const content = await page.content()
+            if (url.includes("detail.m.tmall.com")) {
+                try {
+                    objItem = await service.findTianMao(content)
+                    error = false
+                } catch (error) {
+                    error = true
+                }
+            } else {
+                try {
+                    objItem = await service.findTaobao(page, content, url)
+                    error = false
+                } catch (err) {
+                    error = true
+                }
+            }
+
+            const data = {
+                error,
+                data: objItem
+            }
+
+            service.resJosn(data, res)
+        })();
+    }
+});
+
+const server = app.listen(8521, function () {
+    const host = server.address().address;
+    const port = server.address().port;
+    console.log(host)
+    console.log('success', host, port);
+});
---
+++ service.js
@@ -0,0 +1,209 @@
+// const puppeteer = require('puppeteer');
+const cheerio = require("cheerio");
+// 爬取淘宝
+exports.findTaobao = async (page, content, url) => {
+    const objItem = {
+        swiperImgs: [],
+        title: "",
+        price: "",
+        sku: [],
+        content: [],
+    }
+    try {
+        objItem.title = await getTitle(page)
+        objItem.sku = await getSku(content, url);
+        objItem.swiperImgs = await getSwiper(content)
+        objItem.price = await getPrice(page)
+        objItem.content = await getContent(content)
+        error = false
+    } catch (err) {
+        error = true
+    }
+    return objItem
+}
+// 爬取天猫h5
+exports.findTianMao = (html) => {
+    const objItem = {
+        swiperImgs: [],
+        title: "",
+        price: "",
+        sku: [],
+        content: [],
+    }
+    const $ = cheerio.load(html);
+    //价格
+    $(".price").each(function (i, e) {
+        if (e.childNodes.length && e.childNodes[0].data) {
+            objItem.price = e.childNodes[0].data
+            return false
+        }
+    });
+    // 标题
+    $(".share-warp .main").each(function (i, e) {
+        if (e.childNodes.length && e.childNodes[0].data) {
+            objItem.title = e.childNodes[0].data
+            return false
+        }
+    })
+    // 轮播图
+    $(".preview-scroller a img").each(function (i, e) {
+        if (e.attribs["data-src"]) {
+            objItem.swiperImgs.push(e.attribs["data-src"])
+        }
+    })
+
+    //详情
+    $(".mui-custommodule .mui-custommodule-item img").each(function (i, e) {
+        if (e.attribs["data-ks-lazyload"]) {
+            objItem.content.push(e.attribs["data-ks-lazyload"])
+        }
+    })
+    return objItem
+}
+
+
+exports.changeUrl = (url) => {
+    if (/detail.tmall.com/.test(url)) {
+        url = url.replace("detail.tmall.com", "detail.m.tmall.com")
+    }
+    return url
+}
+
+exports.resJosn = (vm, res) => {
+    res.writeHead(200, {
+        "Content-Type": "application/json"
+    });
+    res.end(JSON.stringify(vm));
+}
+// 获取规格
+const getSku = (html, goodPage) => {
+    const $ = cheerio.load(html);
+    const value2label = {};
+    $(".J_TSaleProp li").each(function (i, elem) {
+        const value = $(this).attr("data-value");
+        const label = $(this)
+            .find("span")
+            .text();
+        value2label[value] = label;
+    });
+    // 预定义结果 skuList = [{name,skuId,stock},{name,skuId,stock}]
+    const skuList = [];
+    let skuMaps;
+    if (/item.taobao.com/.test(goodPage)) {
+        skuMaps = html.match(/skuMap[\s]+:[\s]+{[\S]+[\s]+,propertyMemoMap/);
+        if (skuMaps) {
+            skuMaps = trim(skuMaps[0]);
+            skuMaps = skuMaps.slice(13, -16);
+            skuMaps = JSON.parse(skuMaps);
+        }
+    }
+    if (/detail.tmall.com/.test(goodPage)) {
+        skuMaps = html.match(/"skuMap":{[\S]+,"salesProp"/);
+        if (skuMaps) {
+            skuMaps = JSON.parse(skuMaps[0].slice(9, -12));
+        }
+    }
+    if (skuMaps) {
+        Object.keys(skuMaps).map(key => {
+            const keyArray = key.split(";");
+            let name = "";
+            keyArray.map(i => {
+                if (value2label[i]) name += value2label[i] + " ";
+            });
+            skuList.push({
+                name,
+                skuId: skuMaps[key].skuId,
+                stock: skuMaps[key].stock,
+                price: skuMaps[key].price,
+            });
+        });
+        return skuList;
+    }
+    return [];
+}
+//获取标题
+const getTitle = async (page) => {
+    const t1 = ".tpl-wrapper > div > div > div > span"
+    const t2 = ".tb-detail-hd > h1"
+    const t3 = ".tb-main-title"
+    let title
+    try {
+        title = await page.$eval(t3, ele => ele.innerHTML);
+    } catch (err) {
+        try {
+            title = await page.$eval(t2, ele => ele.innerHTML);
+        } catch (errmsg) {
+            try {
+                title = await page.$eval(t1, ele => ele.innerHTML);
+            } catch (errmsg) {
+                title = "";
+            }
+        }
+    }
+    return trim(title)
+}
+//获取swiper
+const getSwiper = (html) => {
+    let urls = []
+    urls = catchSwiper(".tb-thumb li .tb-pic a img", html)
+    urls = urlSize(urls, "50x50.jpg")
+    // if (/item.taobao.com/.test(url)) {
+    //     urls = catchSwiper(".tb-thumb li .tb-pic a img", html)
+    //     urls = urlSize(urls, "50x50.jpg")
+    // }
+    // if (/detail.tmall.com/.test(url)) {
+    //     urls = catchSwiper(".tb-thumb li a img", html)
+    //     urls = urlSize(urls, "60x60q90.jpg")
+    // }
+
+    return urls
+}
+//获取价格
+const getPrice = async (page) => {
+    let price = ""
+    try {
+        price = await page.$eval('.tb-rmb-num', ele => ele.innerHTML);
+    } catch (error) {
+        price = ""
+    }
+    return price
+}
+
+const getContent = async (html) => {
+    let urls = []
+    urls = catchSwiper(".J_DetailSection .content img", html)
+    return urls
+}
+
+const catchSwiper = (ele, html) => {
+    const $ = cheerio.load(html);
+    let urls = []
+    try {
+        $(ele).each(function (i, elem) {
+            if (elem.attribs.src) {
+                urls.push(elem.attribs.src)
+            }
+        });
+    } catch (err) {
+        console.log(err)
+    }
+    return urls
+}
+
+
+const urlSize = (imgs, size) => {
+    if (!imgs.length) {
+        return imgs
+    }
+    for (let i = 0, len = imgs.length; i < len; i++) {
+        if (imgs[i].includes(size)) {
+            imgs[i] = imgs[i].replace(size, "400x400.jpg")
+        }
+    }
+    return imgs
+}
+
+//去空格
+const trim = (str) => {
+    return str.replace(/^(\s|\u00A0)+/, "").replace(/(\s|\u00A0)+$/, "");
+}
